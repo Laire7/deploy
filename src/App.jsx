bd6a94966fbd87615c613d168182f930939b742e
Laire7 <syoungkwak@gmail.com>
2024-07-02 01:12:25 +0900

    seocho2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FaMinus, FaPlus } from "react-icons/fa";
+import "./App.css";
+import My from "./components/My";
+import { useCount } from "./hooks/counter-context";
+import { SessionProvider } from "./hooks/session-context";
+import Button from "./components/atoms/Button";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { count, plusCount, minusCount } = useCount();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <SessionProvider>
+        <My />
+      </SessionProvider>
+      <div className="border-2 border-red-300 p-3">
+        <h1>count is {count}</h1>
+        <Button text={<FaPlus />} onClick={() => plusCount()} />
+        <Button text={<FaMinus />} onClick={() => minusCount(2)} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
